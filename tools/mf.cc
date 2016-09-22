4291e8358895ea9efe436c807e55a6b6ca873c9a
Olivier <coolivie@amazon.com>
2016-09-22 14:45:27 -0700

    Code formatting fixes

--- tools/mf.cc
+++ tools/mf.cc
@@ -4,32 +4,32 @@
 #include "../src/admf.h"
 
 void show_help() {
-    printf("Usage:\n");
-    printf("./mf\n");
-    printf("--train      xxx       : xxx is the file name of the binary training data.\n");
-    printf("--nu         int       : number of users.\n");
-    printf("--nv         int       : number of items.\n");
-    printf("--test       xxx       : xxx is the file name of the binary test data.\n");
-    printf("--valid      [xxx]     : xxx is the file name of the binary validation data.\n");
-    printf("--result     [xxx]     : save your model in name xxx.\n");
-    printf("--model      [xxx]     : read your model in name xxx.\n");
-    printf("--alg        [xxx]     : xxx can be {mf, dpmf, admf}.\n");
-    printf("--dim        [int]     : low rank of the model.\n");
-    printf("--iter       [int]     : number of iterations.\n");
-    printf("--fly        [int]     : number of threads.\n");
-    printf("--stride     [int]     : prefetch strides.\n");
-    printf("--eta        [float]   : learning rate.\n");
-    printf("--lambda     [float]   : regularizer.\n");
-    printf("--gam        [float]   : decay of learning rate.\n");
-    printf("--bias       [float]   : global bias (important for accuracy).\n");
-    printf("--mineta     [float]   : minimum learning rate (sometimes used in SGLD).\n");
-    printf("--epsilon    [float]   : sensitivity of differentially privacy.\n");
-    printf("--tau        [int]     : maximum of ratings among all the users (usually after trimming your data).\n");
-    printf("--temp       [float]   : temprature in SGLD (can accelarate the convergence).\n");
-    printf("--noise_size [int]     : the Gaussian numbers lookup table.\n");
-    printf("--eta_reg    [float]   : the learning rate for estimating regularization parameters.\n");
-    printf("--loss       [int]     : the loss type can be {least square, 0-1 logistic regression}.\n");
-    printf("--measure    [int]     : support RMSE.\n");
+  printf("Usage:\n");
+  printf("./mf\n");
+  printf("--train      xxx       : xxx is the file name of the binary training data.\n");
+  printf("--nu         int       : number of users.\n");
+  printf("--nv         int       : number of items.\n");
+  printf("--test       xxx       : xxx is the file name of the binary test data.\n");
+  printf("--valid      [xxx]     : xxx is the file name of the binary validation data.\n");
+  printf("--result     [xxx]     : save your model in name xxx.\n");
+  printf("--model      [xxx]     : read your model in name xxx.\n");
+  printf("--alg        [xxx]     : xxx can be {mf, dpmf, admf}.\n");
+  printf("--dim        [int]     : low rank of the model.\n");
+  printf("--iter       [int]     : number of iterations.\n");
+  printf("--fly        [int]     : number of threads.\n");
+  printf("--stride     [int]     : prefetch strides.\n");
+  printf("--eta        [float]   : learning rate.\n");
+  printf("--lambda     [float]   : regularizer.\n");
+  printf("--gam        [float]   : decay of learning rate.\n");
+  printf("--bias       [float]   : global bias (important for accuracy).\n");
+  printf("--mineta     [float]   : minimum learning rate (sometimes used in SGLD).\n");
+  printf("--epsilon    [float]   : sensitivity of differentially privacy.\n");
+  printf("--tau        [int]     : maximum of ratings among all the users (usually after trimming your data).\n");
+  printf("--temp       [float]   : temprature in SGLD (can accelarate the convergence).\n");
+  printf("--noise_size [int]     : the Gaussian numbers lookup table.\n");
+  printf("--eta_reg    [float]   : the learning rate for estimating regularization parameters.\n");
+  printf("--loss       [int]     : the loss type can be {least square, 0-1 logistic regression}.\n");
+  printf("--measure    [int]     : support RMSE.\n");
 }
 
 //assuming test data can fit into RAM
@@ -67,8 +67,8 @@ void run(DPMF& dpmf) {
   p.add_filter(sgld_f);
   s = Time::now();
   for(int i=1; i<=dpmf.iter_; i++) {
-      p.run(dpmf.data_in_fly_);
-      dpmf.finish_round(blocks_test, i);
+    p.run(dpmf.data_in_fly_);
+    dpmf.finish_round(blocks_test, i);
   }
   fclose(f);
 }
@@ -93,72 +93,72 @@ void run(AdaptRegMF& admf) {
 }
 
 int main(int argc, char** argv) {
-    char *train_data=NULL, *test_data=NULL, *result=NULL, *alg=NULL, *model=NULL;
-    int dim = 128, iter = 15, tau=0, nu=0, nv=0, fly=8, stride=2;
-    float eta = 2e-2, lambda = 5e-3, gam=1.0f, mineta=1e-13;
-    float epsilon=0.0f, hypera=1.0f, hyperb=100.0f, temp=1.0f;
-    float g_bias = 2.76f;
-    int noise_size = 2000000000;
-    int loss = 0;
-    int measure = 0;
-    float eta_reg = 2e-3f;
-    char* valid_data=NULL;
-    for(int i = 1; i < argc; i++) {
-        if(!strcmp(argv[i], "--train"))            train_data = argv[++i];
-        else if(!strcmp(argv[i], "--test"))        test_data = argv[++i];
-        else if(!strcmp(argv[i], "--valid"))       valid_data = argv[++i];
-        else if(!strcmp(argv[i], "--result"))      result = argv[++i];
-        else if(!strcmp(argv[i], "--model"))       model = argv[++i];
-        else if(!strcmp(argv[i], "--alg"))         alg = argv[++i];
-        else if(!strcmp(argv[i], "--dim"))         dim  = atoi(argv[++i]);
-        else if(!strcmp(argv[i], "--iter"))        iter = atoi(argv[++i]);
-        else if(!strcmp(argv[i], "--nu"))          nu  = atoi(argv[++i]);
-        else if(!strcmp(argv[i], "--nv"))          nv  = atoi(argv[++i]);
-        else if(!strcmp(argv[i], "--fly"))         fly  = atoi(argv[++i]);
-        else if(!strcmp(argv[i], "--stride"))      stride  = atoi(argv[++i]);
-        else if(!strcmp(argv[i], "--eta"))         eta = atof(argv[++i]);
-        else if(!strcmp(argv[i], "--lambda"))      lambda = atof(argv[++i]);
-        else if(!strcmp(argv[i], "--gam"))         gam = atof(argv[++i]);
-        else if(!strcmp(argv[i], "--bias"))        g_bias = atof(argv[++i]);
-        else if(!strcmp(argv[i], "--mineta"))      mineta = atof(argv[++i]);
-        else if(!strcmp(argv[i], "--epsilon"))     epsilon = atof(argv[++i]);
-        else if(!strcmp(argv[i], "--tau"))         tau = atoi(argv[++i]);
-        else if(!strcmp(argv[i], "--hypera"))      hypera = atof(argv[++i]);
-        else if(!strcmp(argv[i], "--hyperb"))      hyperb = atof(argv[++i]);
-        else if(!strcmp(argv[i], "--temp"))        temp = atof(argv[++i]);
-        else if(!strcmp(argv[i], "--noise_size"))  noise_size  = atoi(argv[++i]);
-        else if(!strcmp(argv[i], "--eta_reg"))     eta_reg = atof(argv[++i]);
-        else if(!strcmp(argv[i], "--loss"))        loss  = atoi(argv[++i]);
-        else if(!strcmp(argv[i], "--measure"))     measure  = atoi(argv[++i]);
-        else {
-            printf("%s, unknown parameters, exit\n", argv[i]);
-            exit(1);
-        }
+  char *train_data=NULL, *test_data=NULL, *result=NULL, *alg=NULL, *model=NULL;
+  int dim = 128, iter = 15, tau=0, nu=0, nv=0, fly=8, stride=2;
+  float eta = 2e-2, lambda = 5e-3, gam=1.0f, mineta=1e-13;
+  float epsilon=0.0f, hypera=1.0f, hyperb=100.0f, temp=1.0f;
+  float g_bias = 2.76f;
+  int noise_size = 2000000000;
+  int loss = 0;
+  int measure = 0;
+  float eta_reg = 2e-3f;
+  char* valid_data=NULL;
+  for(int i = 1; i < argc; i++) {
+    if(!strcmp(argv[i], "--train"))            train_data = argv[++i];
+    else if(!strcmp(argv[i], "--test"))        test_data = argv[++i];
+    else if(!strcmp(argv[i], "--valid"))       valid_data = argv[++i];
+    else if(!strcmp(argv[i], "--result"))      result = argv[++i];
+    else if(!strcmp(argv[i], "--model"))       model = argv[++i];
+    else if(!strcmp(argv[i], "--alg"))         alg = argv[++i];
+    else if(!strcmp(argv[i], "--dim"))         dim  = atoi(argv[++i]);
+    else if(!strcmp(argv[i], "--iter"))        iter = atoi(argv[++i]);
+    else if(!strcmp(argv[i], "--nu"))          nu  = atoi(argv[++i]);
+    else if(!strcmp(argv[i], "--nv"))          nv  = atoi(argv[++i]);
+    else if(!strcmp(argv[i], "--fly"))         fly  = atoi(argv[++i]);
+    else if(!strcmp(argv[i], "--stride"))      stride  = atoi(argv[++i]);
+    else if(!strcmp(argv[i], "--eta"))         eta = atof(argv[++i]);
+    else if(!strcmp(argv[i], "--lambda"))      lambda = atof(argv[++i]);
+    else if(!strcmp(argv[i], "--gam"))         gam = atof(argv[++i]);
+    else if(!strcmp(argv[i], "--bias"))        g_bias = atof(argv[++i]);
+    else if(!strcmp(argv[i], "--mineta"))      mineta = atof(argv[++i]);
+    else if(!strcmp(argv[i], "--epsilon"))     epsilon = atof(argv[++i]);
+    else if(!strcmp(argv[i], "--tau"))         tau = atoi(argv[++i]);
+    else if(!strcmp(argv[i], "--hypera"))      hypera = atof(argv[++i]);
+    else if(!strcmp(argv[i], "--hyperb"))      hyperb = atof(argv[++i]);
+    else if(!strcmp(argv[i], "--temp"))        temp = atof(argv[++i]);
+    else if(!strcmp(argv[i], "--noise_size"))  noise_size  = atoi(argv[++i]);
+    else if(!strcmp(argv[i], "--eta_reg"))     eta_reg = atof(argv[++i]);
+    else if(!strcmp(argv[i], "--loss"))        loss  = atoi(argv[++i]);
+    else if(!strcmp(argv[i], "--measure"))     measure  = atoi(argv[++i]);
+    else {
+      printf("%s, unknown parameters, exit\n", argv[i]);
+      exit(1);
     }
-    if (train_data == NULL || nu == 0 || nv == 0) {
-        printf("Note that train_data/#users/#items are not optional!\n");
-        show_help();
-        exit(1);
-    }
-    if(!strcmp(alg, "mf") || alg==NULL) {
-        MF mf(train_data, test_data, result, model, dim, iter, eta, gam, lambda, \
+  }
+  if (train_data == NULL || nu == 0 || nv == 0) {
+    printf("Note that train_data/#users/#items are not optional!\n");
+    show_help();
+    exit(1);
+  }
+  if(!strcmp(alg, "mf") || alg==NULL) {
+    MF mf(train_data, test_data, result, model, dim, iter, eta, gam, lambda, \
               g_bias, nu, nv, fly, stride);
-        run(mf);
-    }
-    else if (!strcmp(alg, "dpmf")) {
-        DPMF dpmf(train_data, test_data, result, model, dim, iter, eta, gam, lambda, \
+    run(mf);
+  }
+  else if (!strcmp(alg, "dpmf")) {
+    DPMF dpmf(train_data, test_data, result, model, dim, iter, eta, gam, lambda, \
                   g_bias, nu, nv, fly, stride, hypera, hyperb, epsilon, tau, \
                   noise_size, temp, mineta);
-        run(dpmf);
-    }
-    else if (!strcmp(alg, "admf")) {
-        AdaptRegMF admf(train_data, test_data, valid_data, result, model, dim, iter, eta, gam, \
+    run(dpmf);
+  }
+  else if (!strcmp(alg, "admf")) {
+    AdaptRegMF admf(train_data, test_data, valid_data, result, model, dim, iter, eta, gam, \
                         lambda, g_bias, nu, nv, fly, stride, loss, measure, eta_reg);
-        run(admf);
-    }
-    else {
-        printf("Pleae select a solver: mf/dpmf/admf\n");
-        exit(2);
-    }
-    return 0;
+    run(admf);
+  }
+  else {
+    printf("Pleae select a solver: mf/dpmf/admf\n");
+    exit(2);
+  }
+  return 0;
 }
